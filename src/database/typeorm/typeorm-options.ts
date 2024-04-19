6faf45dcc2cea20c6e3bdb5c6094832118d33df6
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-04-19 19:36:04 -0300

    refactor: typeorm options entities path

--- src/database/typeorm/typeorm-options.ts
+++ src/database/typeorm/typeorm-options.ts
@@ -1,12 +1,10 @@
 import { DataSourceOptions } from 'typeorm';
 
-import { Todo } from '../../domain/entities/todo.entity';
-
 export const typeormOptions: DataSourceOptions = {
   type: 'sqlite',
   database: 'db.sqlite',
   dropSchema: true,
   synchronize: true,
   logging: false,
-  entities: [Todo],
+  entities: ['src/**/*.entity.{ts,js}'],
 };
