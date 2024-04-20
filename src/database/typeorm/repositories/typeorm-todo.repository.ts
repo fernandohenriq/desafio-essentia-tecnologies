8aa1d9f8563d86b7d618f5df11ff1c66c84bbc7c
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-04-19 21:35:43 -0300

    refactor: typeorm todo repository

--- src/database/typeorm/repositories/typeorm-todo.repository.ts
+++ src/database/typeorm/repositories/typeorm-todo.repository.ts
@@ -7,11 +7,37 @@ const typeormManager = new TypeormManager();
 export class TypeOrmTodoRepository implements TodoRepository {
   private repo = typeormManager.getRepository(Todo);
 
-  async save(entity: Todo): Promise<Todo> {
-    return await this.repo.save(entity);
+  async save(todo: Todo): Promise<Todo> {
+    const todoSaved = await this.repo.save(todo);
+    return todoSaved;
   }
 
-  async findById(id: string): Promise<Todo | null> {
-    return await this.repo.findOneBy({ id });
+  async findOne(where: Where<Todo>): Promise<Todo | null> {
+    const todoFound = await this.repo.findOne({ where });
+    if (todoFound === null || todoFound === undefined) {
+      return null;
+    }
+    return todoFound;
+  }
+
+  async findMany(search: Search<Todo>): Promise<Todo[]> {
+    const { page = 1, limit = 10, orderBy = 'createdAt', sort = 'desc', where = {} } = search ?? {};
+    const query = this.repo
+      .createQueryBuilder('todo')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .orderBy(`todo.${orderBy}`, sort.toUpperCase() as 'ASC' | 'DESC');
+    if (Object.keys(where).length > 0) {
+      for (const [key, value] of Object.entries(where)) {
+        query.andWhere(`todo.${key} = :${key}`, { [key]: value });
+      }
+    }
+    const todos = await query.getMany();
+    return todos;
+  }
+
+  async remove(entity: Todo): Promise<void> {
+    await this.repo.remove(entity);
+    return;
   }
 }
