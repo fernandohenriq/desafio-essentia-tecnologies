4cae33eeeee5f88f55349fc5035a2f2516811e3d
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-04-21 15:30:06 -0300

    refactor: typeorm db options

--- src/database/typeorm/typeorm-options.ts
+++ src/database/typeorm/typeorm-options.ts
@@ -1,10 +1,36 @@
 import { DataSourceOptions } from 'typeorm';
 
-export const typeormOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  dropSchema: true,
-  synchronize: true,
-  logging: false,
-  entities: ['src/**/*.entity.{ts,js}'],
-};
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+export const typeormOptions: DataSourceOptions = (() => {
+  const DB_TYPE = process.env.DB_TYPE || 'mysql';
+  const DB_HOST = process.env.DB_HOST || 'localhost';
+  const DB_PORT = process.env.DB_PORT || '3306';
+  const DB_USERNAME = process.env.DB_USERNAME;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  const DB_NAME = process.env.DB_NAME || 'postgres';
+
+  switch (NODE_ENV) {
+    case 'production':
+      return {
+        type: DB_TYPE as any,
+        host: DB_HOST,
+        port: Number(DB_PORT),
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        synchronize: false,
+        logging: false,
+        entities: ['dist/**/*.entity.{ts,js}'],
+      };
+    default:
+      return {
+        type: 'sqlite',
+        database: ':memory:',
+        dropSchema: true,
+        synchronize: true,
+        logging: false,
+        entities: ['src/**/*.entity.{ts,js}'],
+      };
+  }
+})();
