3262d53435ce3462d6bd4d283c338597b631689c
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-04-22 15:25:58 -0300

    refactor: change put to patch cors method

--- server/src/presentation/express-app.ts
+++ server/src/presentation/express-app.ts
@@ -15,7 +15,7 @@ export class ExpressApp {
     this.app.use(express.json());
     this.app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
       res.header('Access-Control-Allow-Headers', 'Content-Type');
       next();
     });
